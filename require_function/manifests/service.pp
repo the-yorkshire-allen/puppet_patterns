176788054fc7a075f671ee28f0136e4e90addeb1
Chris Allen <chris.allen@puppet.com>
2023-01-18 09:53:17 +0000

    Moved require function parameter to a dependencies Array on Class parameter

--- require_function/manifests/service.pp
+++ require_function/manifests/service.pp
@@ -1,11 +1,15 @@
 # @summary A short summary of the purpose of this class
 #
 # A description of what this class does
+# @param
+#   dependencies - An array of dependencies to require
 #
 # @example
 #   include require_function::service
-class require_function::service {
-  require require_function::setup
+class require_function::service (
+  Array[String] $dependencies = ['require_function::setup'],
+) {
+  require dependencies
 
   service { 'docker':
     ensure => running,
